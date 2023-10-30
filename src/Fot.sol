de269ccfb72613239415bfd19f46e4f5e7d980dd
alex <enna@mailbox.org>
2023-10-30 17:06:34 +0100

    Adding Fot.sol

---
+++ src/Fot.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract Fot is ERC20 {
+    uint256 public transferFeePercentage = 1; // 1% fee
+
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal override {
+        uint256 fee = (amount * transferFeePercentage) / 100;
+        uint256 amountAfterFee = amount - fee;
+
+        super._transfer(sender, recipient, amountAfterFee);
+        super._transfer(sender, address(this), fee); // Transfer fee to the contract
+    }
+}
