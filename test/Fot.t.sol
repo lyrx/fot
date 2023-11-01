7bd787f61dcee2412087f41bec5d944abe864821
alex <enna@mailbox.org>
2023-11-01 10:56:51 +0100

    Improve to get 100% coverage!

--- test/Fot.t.sol
+++ test/Fot.t.sol
@@ -6,14 +6,12 @@ import "../src/Fot.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "forge-std/Test.sol";
 
-
-
 contract FotTest is DSTest, Test {
     Fot fot;
     address alice = address(0x1);
     address bob = address(0x2);
     address feeRecipient = address(0x3);
-    address nullAddress=address(0x0);
+    address nullAddress = address(0x0);
     uint256 constant INITIAL_SUPPLY = 1000 * 10 ** 18;
     uint256 constant TRANSFER_AMOUNT = 200 * 10 ** 18;
 
@@ -30,13 +28,11 @@ contract FotTest is DSTest, Test {
     }
 
     function assertInitialBalances() public {
-        uint256 fee = (TRANSFER_AMOUNT * fot.transferFeePercentage()) / 100;
-        uint256 amountAfterFee = TRANSFER_AMOUNT - fee;
+        (uint256 amountAfterFee,) = calculateFeeAndAmountAfterFee(TRANSFER_AMOUNT);
 
         uint256 actualBalanceThis = fot.balanceOf(address(this));
         uint256 actualBalanceAlice = fot.balanceOf(alice);
         uint256 actualBalanceBob = fot.balanceOf(bob);
-
 
         assertEq(actualBalanceThis, INITIAL_SUPPLY - 2 * TRANSFER_AMOUNT);
         assertEq(actualBalanceAlice, amountAfterFee);
@@ -48,52 +44,48 @@ contract FotTest is DSTest, Test {
     }
 
     function testTransferFromWithFee() public {
-        uint256 aliceInitialBalance = fot.balanceOf(alice);
-        uint256 bobInitialBalance = fot.balanceOf(bob);
-        uint256 feeRecipientInitialBalance = fot.balanceOf(feeRecipient); // Check fee recipient balance
-
-        uint256 amount = TRANSFER_AMOUNT / 2;
-        uint256 fee = (amount * fot.transferFeePercentage()) / 100;
-        uint256 amountAfterFee = amount - fee;
-
-        fot.transferFrom(alice, bob, amount);
-
-        assertEq(fot.balanceOf(alice), aliceInitialBalance - amount);
-        assertEq(fot.balanceOf(bob), bobInitialBalance + amountAfterFee);
-        assertEq(fot.balanceOf(feeRecipient), feeRecipientInitialBalance + fee); // Check fee recipient balance
+        testTransferWithFeeInternal(alice, bob, TRANSFER_AMOUNT / 2,true);
     }
 
     function testTransferWithFee() public {
-        uint256 aliceInitialBalance = fot.balanceOf(alice);
-        uint256 bobInitialBalance = fot.balanceOf(bob);
-        uint256 feeRecipientInitialBalance = fot.balanceOf(feeRecipient); // Check fee recipient balance
 
-        uint256 amount = TRANSFER_AMOUNT / 2;
-        uint256 fee = (amount * fot.transferFeePercentage()) / 100;
-        uint256 amountAfterFee = amount - fee;
-
-        vm.prank(alice);
-        fot.transfer( bob, amount);
-
-        assertEq(fot.balanceOf(alice), aliceInitialBalance - amount);
-        assertEq(fot.balanceOf(bob), bobInitialBalance + amountAfterFee);
-        assertEq(fot.balanceOf(feeRecipient), feeRecipientInitialBalance + fee); // Check fee recipient balance
+        testTransferWithFeeInternal(alice, bob, TRANSFER_AMOUNT / 2,false);
     }
 
+    function testTransferWithFeeInternal(address from, address to, uint256 amount, bool useFrom) internal {
+        uint256 initialBalanceFrom = fot.balanceOf(from);
+        uint256 initialBalanceTo = fot.balanceOf(to);
+        uint256 initialBalanceFeeRecipient = fot.balanceOf(feeRecipient);
+
+        (uint256 amountAfterFee, uint256 fee) = calculateFeeAndAmountAfterFee(amount);
+
+        if(useFrom){
+            fot.transferFrom(from, to, amount);
+        }
+        else {
+            vm.prank(from);
+            fot.transfer(to, amount);
+        }
 
 
+        assertEq(fot.balanceOf(from), initialBalanceFrom - amount);
+        assertEq(fot.balanceOf(to), initialBalanceTo + amountAfterFee);
+        assertEq(fot.balanceOf(feeRecipient), initialBalanceFeeRecipient + fee);
+    }
 
+    function calculateFeeAndAmountAfterFee(uint256 amount) internal view returns (uint256 amountAfterFee, uint256 fee) {
+        fee = (amount * fot.transferFeePercentage()) / 100;
+        amountAfterFee = amount - fee;
+        return (amountAfterFee, fee);
+    }
 
     function testFailTransferMoreThanBalance() public {
         fot.transferFrom(alice, bob, INITIAL_SUPPLY);
         assertInitialBalances();
     }
 
-
     function testNullAddress() public {
         vm.expectRevert();
         fot.transferFrom(nullAddress, nullAddress, INITIAL_SUPPLY);
-
     }
-
 }
