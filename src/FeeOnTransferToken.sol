f8e439b10cbebae279164f667ffecfeff701fd6d
alex <enna@mailbox.org>
2023-11-02 12:20:35 +0100

    Fix empty lines in contract

--- src/FeeOnTransferToken.sol
+++ src/FeeOnTransferToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
 import {ERC20} from  "@openzeppelin-contracts@5.0.0/token/ERC20/ERC20.sol";
 
 /// @title Marco's cool fee on transfer token
