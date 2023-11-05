c0d3f6031562586009daa61c69f915db97e70382
alex <enna@mailbox.org>
2023-11-05 16:57:09 +0100

    Revert "Revert "Revert "Refactor to use delegates"""
    This reverts commit de2627e6b31556a4b3d2ab8edf7484b7de93a013.

--- src/FeeOnTransferToken.sol
+++ src/FeeOnTransferToken.sol
@@ -1,76 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
-import {IERC20} from  "@openzeppelin-contracts@5.0.0/token/ERC20/IERC20.sol";
+import {ERC20} from  "@openzeppelin-contracts@5.0.0/token/ERC20/ERC20.sol";
 
 /// @title Marco's cool fee on transfer token
-/// @notice This contract delegates transfer calls to another ERC20 token while maintaining ERC20 interface
-contract FeeOnTransferToken {
+/// @author Alexander Weinmann
+/// @notice This contract implements a token with a transfer fee
+/// @dev Inherits from OpenZeppelin's ERC20 contract
+contract FeeOnTransferToken is ERC20 {
     uint256 private constant BASIS_POINTS_DENOMINATOR = 100;
+    uint256 private constant DEFAULT_TRANSFER_FEE_PERCENTAGE = 1; // 1% fee
+    uint256 public transferFeePercentage;
     address public feeRecipientAddress; // Address to receive the fee
-    IERC20 public delegateToken;
 
     /// @notice Creates a new Fot token
-    /// @param _delegateTokenAddress The ERC20 token address to delegate transfers to
+    /// @param _initialSupply The initial supply of tokens
     /// @param _feeRecipientAddress The address to receive the fee
     /// @param _transferFeePercentage The transfer fee percentage
-    constructor(
-        address _delegateTokenAddress,
-        address _feeRecipientAddress,
-        uint256 _transferFeePercentage
-    )  {
-        require(_delegateTokenAddress != address(0), "FeeOnTransferToken: Delegate token cannot be the zero address");
+    /// @dev The constructor sets the initial supply, fee recipient address, and transfer fee percentage
+    constructor(uint256 _initialSupply, address _feeRecipientAddress, uint256 _transferFeePercentage) ERC20("Marco's cool fee on transfer token", "FOT") {
         require(_feeRecipientAddress != address(0), "FeeOnTransferToken: Fee recipient cannot be the zero address");
-
-        delegateToken = IERC20(_delegateTokenAddress);
-        feeRecipientAddress = _feeRecipientAddress;
-        transferFeePercentage = _transferFeePercentage;
+        _mint(msg.sender, _initialSupply);
+        feeRecipientAddress = _feeRecipientAddress; // Set the fee recipient address
+        transferFeePercentage = _transferFeePercentage == 0 ? DEFAULT_TRANSFER_FEE_PERCENTAGE : _transferFeePercentage; // Set the transfer fee percentage
     }
 
-    uint256 public transferFeePercentage;
-
-    /// @notice Delegate transfer to another ERC20 token with fee
+    /// @notice Transfer tokens with fee
     /// @param _recipient The address receiving the tokens
     /// @param _amount The amount of tokens being transferred
     /// @return success True if the transfer was successful
-    function transfer(address _recipient, uint256 _amount) public returns (bool success) {
+    function transfer(address _recipient, uint256 _amount) public override returns (bool success) {
+        require(_amount > 0, "FeeOnTransferToken: Transfer amount must be greater than zero");
         uint256 fee = (_amount * transferFeePercentage) / BASIS_POINTS_DENOMINATOR;
         uint256 amountAfterFee = _amount - fee;
-
-        delegateToken.transfer(_recipient, amountAfterFee);
-        delegateToken.transfer(feeRecipientAddress, fee);
-
+        super.transfer(_recipient, amountAfterFee);
+        super.transfer(feeRecipientAddress, fee);
         return true;
     }
 
-    /// @notice Delegate transferFrom to another ERC20 token with fee
+    /// @notice Transfer tokens from one address to another with fee
     /// @param _sender The address sending the tokens
     /// @param _recipient The address receiving the tokens
     /// @param _amount The amount of tokens being transferred
     /// @return success True if the transfer was successful
-    function transferFrom(address _sender, address _recipient, uint256 _amount) public returns (bool success) {
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool success) {
+        require(_amount > 0, "FeeOnTransferToken: Transfer amount must be greater than zero");
         uint256 fee = (_amount * transferFeePercentage) / BASIS_POINTS_DENOMINATOR;
         uint256 amountAfterFee = _amount - fee;
-
-        delegateToken.transferFrom(_sender, _recipient, amountAfterFee);
-        delegateToken.transferFrom(_sender, feeRecipientAddress, fee);
-
+        super.transferFrom(_sender, _recipient, amountAfterFee);
+        super.transferFrom(_sender, feeRecipientAddress, fee);
         return true;
     }
-
-    /// @notice Gets the total supply of the delegated token
-    /// @return The total supply of the delegated token
-    function totalSupply() public view returns (uint256) {
-        return delegateToken.totalSupply();
-    }
-
-    /// @notice Get the balance of an account
-    /// @param account The address of the token holder
-    /// @return The balance of the account
-    function balanceOf(address account) public view returns (uint256) {
-        return delegateToken.balanceOf(account);
-    }
-
-
 }
