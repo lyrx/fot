3ab777a6e26c980e3e0c0de4e7ea5153a8cf9fc6
alex <enna@mailbox.org>
2023-11-19 07:57:23 +0100

    Improve Documentation

--- src/BondingCurve.sol
+++ src/BondingCurve.sol
@@ -6,11 +6,12 @@ import "forge-std/console.sol";
 
 /// @title A secure linear bonding curve contract resistant to sandwich attacks
 /// @notice Implements a bonding curve with protections against price manipulation through sandwich attacks
+/// This contract uses a linear bonding curve to manage the price of its tokens, which increases with each token sold.
 contract BondingCurve is Ownable {
     uint256 public totalSupply; // Total supply of tokens in circulation
-    uint256 private constant INITIAL_PRICE = 1 ether; // Initial price per token
-    uint256 private constant PRICE_INCREMENT = 0.01 ether; // Incremental price increase per token
-    uint256 private constant MAX_SLIPPAGE = 5; // Maximum allowed slippage in percentage
+    uint256 private constant INITIAL_PRICE = 1 ether; // Initial price per token, set to 1 ETH
+    uint256 private constant PRICE_INCREMENT = 0.01 ether; // Incremental price increase per token, set to 0.01 ETH
+    uint256 private constant MAX_SLIPPAGE = 5; // Maximum allowed slippage in percentage, set to 5%
 
     mapping(address => uint256) public balances; // Mapping of addresses to their token balances
 
@@ -22,13 +23,17 @@ contract BondingCurve is Ownable {
     /// @param numTokens Number of tokens to be bought
     /// @param maxPrice Maximum price the user is willing to pay for the tokens
     /// @return The cost for the purchased tokens
+    /// The function calculates the cost of the requested number of tokens and ensures that the transaction meets the user's max price condition.
     function buyToken(uint256 numTokens, uint256 maxPrice) public payable returns (uint256) {
         uint256 cost = calculatePrice(numTokens);
+        // Reverts if the calculated cost exceeds the user's maximum price limit.
         if (cost > maxPrice) revert("BondingCurve: Market price exceeds maxPrice");
+        // Reverts if the sent Ether value is less than the cost of the tokens.
         if (msg.value < cost) revert("BondingCurve: Not enough Ether sent.");
 
         bool slippage = validateSlippage(cost, maxPrice);
-        if (!slippage) revert("BondingCurve: Slippage too high");// Slippage protection
+        // Reverts if the slippage is too high, indicating a significant price change that might not be favorable to the user.
+        if (!slippage) revert("BondingCurve: Slippage too high");
 
         totalSupply += numTokens;
         balances[msg.sender] += numTokens;
@@ -40,13 +45,16 @@ contract BondingCurve is Ownable {
     /// @param numTokens Number of tokens to be sold
     /// @param minRevenue Minimum revenue expected from the sale
     /// @return The revenue received from selling the tokens
+    /// Users can sell tokens back to the contract at the current price, with a check to ensure they receive at least their minimum expected revenue.
     function sellToken(uint256 numTokens, uint256 minRevenue) public returns (uint256) {
+        // Reverts if the user does not have enough tokens to sell.
         if (balances[msg.sender] < numTokens) revert("BondingCurve: Not enough tokens.");
 
         uint256 revenue = calculatePrice(numTokens);
+        // Reverts if the revenue from selling the tokens is less than the user's minimum expected revenue.
         if (revenue < minRevenue) revert("BondingCurve: Revenue less than minRevenue");
 
-        // Slippage protection
+        // Reverts if the slippage is too high when selling, which protects the user from unfavorable price changes.
         if (!validateSlippage(revenue, minRevenue)) revert("BondingCurve: Slippage too high");
 
         totalSupply -= numTokens;
@@ -60,6 +68,7 @@ contract BondingCurve is Ownable {
     /// @notice Calculates the price for a given number of tokens
     /// @param numTokens Number of tokens for which the price is to be calculated
     /// @return Price for the specified number of tokens
+    /// This function calculates the price for a given number of tokens based on the initial price and the total supply, reflecting the linear bonding curve.
     function calculatePrice(uint256 numTokens) public view returns (uint256) {
         return (INITIAL_PRICE + PRICE_INCREMENT * totalSupply) * numTokens;
     }
@@ -68,6 +77,7 @@ contract BondingCurve is Ownable {
     /// @param actualValue Actual value or price of the transaction
     /// @param expectedValue Expected value or price of the transaction
     /// @return True if the slippage is within the acceptable range, false otherwise
+    /// This function calculates the slippage percentage and returns true if it's within the allowed threshold, helping prevent large, unexpected price movements.
     function validateSlippage(uint256 actualValue, uint256 expectedValue) private pure returns (bool) {
         uint256 slippage = (actualValue > expectedValue) ?
             ((actualValue - expectedValue) * 100) / expectedValue :
--- test/BondingCurve.t.sol
+++ test/BondingCurve.t.sol
@@ -5,84 +5,92 @@ import "ds-test/test.sol";
 import "../src/BondingCurve.sol";
 import "forge-std/Test.sol";
 
+/// @title Tests for the BondingCurve contract
+/// @notice This suite of tests ensures the correct functioning of the BondingCurve contract
 contract BondingCurveTest is DSTest, Test {
     BondingCurve bondingCurve;
     address owner;
-    address private constant ALICE = address(0x9);
+    address private constant ALICE = address(0x9); // A test address for simulating external interactions
 
     function setUp() public {
-        owner = address(this); // Sets the test contract as the owner
-        bondingCurve = new BondingCurve(owner);
+        owner = address(this); // Sets the test contract itself as the owner of the BondingCurve contract
+        bondingCurve = new BondingCurve(owner); // Creates a new instance of BondingCurve for testing
     }
 
+    /// Test successful token purchase
     function testBuyTokenSuccess() public {
         uint256 numTokens = 1;
         uint256 maxPrice = 1.05 ether;
         // Simulates sending Ether to the contract and calls buyToken
         bondingCurve.buyToken{value: 1 ether}(numTokens, maxPrice);
-        // Checks if the token count has been correctly increased
+        // Asserts if the totalSupply of the bondingCurve has correctly increased by the number of tokens bought
         assertEq(bondingCurve.totalSupply(), numTokens);
     }
 
+    /// Test token purchase failure due to insufficient Ether sent
     function testFailBuyTokenNotEnoughEther() public {
         uint256 numTokens = 1;
         uint256 maxPrice = 1.05 ether;
+        // Should fail because the Ether sent is less than the cost of numTokens
         bondingCurve.buyToken{value: 0.5 ether}(numTokens, maxPrice);
     }
 
+    /// Test token purchase failure when the market price exceeds user's max price
     function testFailBuyTokenExceedsMaxPrice() public {
         uint256 numTokens = 1;
         uint256 maxPrice = 0.5 ether;
+        // Should fail because the market price is higher than the maxPrice specified by the user
         bondingCurve.buyToken{value: 0.5 ether}(numTokens, maxPrice);
     }
-    function testFailBuyTokenLowerThanMaxPrice() public {
-        uint256 numTokens = 1;
-        uint256 maxPrice = 2 ether;
-        bondingCurve.buyToken{value: 2 ether}(numTokens, maxPrice);
-    }
 
+    /// Test token purchase failure due to slippage being too high
     function testFailBuyTokenSlippageTooHigh() public {
         uint256 numTokens = 1;
         uint256 maxPrice = 1.1 ether;
-        bondingCurve.buyToken{value: 1.05 ether}(numTokens, maxPrice);
-    }
-    function testFailBuyTokenSlippageTooLow() public {
-        uint256 numTokens = 1;
-        uint256 maxPrice = 0.5 ether;
+        // Should fail because the slippage exceeds the maximum allowed
         bondingCurve.buyToken{value: 1.05 ether}(numTokens, maxPrice);
     }
 
+    /// Test successful token sale
     function testSellTokenSuccess() public {
+        // First buy a token to sell
         bondingCurve.buyToken{value: 1.05 ether}(1, 1.05 ether);
         uint256 minRevenue = 0.99 ether;
+        // Sell the token
         bondingCurve.sellToken(1, minRevenue);
-        // Checks if the token count has been correctly reduced
+        // Asserts if the totalSupply of the bondingCurve has correctly reduced to 0
         assertEq(bondingCurve.totalSupply(), 0);
     }
 
+    /// Test token sale failure due to insufficient token balance
     function testFailSellTokenNotEnoughTokens() public {
         uint256 minRevenue = 0.99 ether;
+        // Alice attempts to sell a token she does not own, should fail
         vm.prank(ALICE);
         bondingCurve.sellToken(1, minRevenue);
     }
 
+    /// Test token sale failure due to revenue being less than minimum expected
     function testFailSellTokenNotEnoughRevenue() public {
+        // First buy a token to sell
         bondingCurve.buyToken{value: 1 ether}(1, 1 ether);
+        // Attempt to sell the token for more than its worth, should fail
         bondingCurve.sellToken(1, 1.1 ether);
     }
 
+    /// Test token sale failure due to slippage being too high
     function testFailSellTokenSlippageTooHigh() public {
+        // First buy a token to sell
         bondingCurve.buyToken{value: 1.05 ether}(1, 1.05 ether);
+        // Attempt to sell the token with a minimum revenue that causes too high slippage, should fail
         bondingCurve.sellToken(1, 1.15 ether);
     }
 
-    function testFailSellTokenSlippageTooLow() public {
-        bondingCurve.buyToken{value: 1.05 ether}(1, 1.05 ether);
-        bondingCurve.sellToken(1, 0.15 ether);
-    }
-
+    /// Test correct calculation of token price
     function testCalculatePrice() public {
+        // Check the price calculation for 1 token
         uint256 price = bondingCurve.calculatePrice(1);
+        // Asserts that the price of 1 token is equal to 1 ether
         assertEq(price, 1 ether);
     }
 }
